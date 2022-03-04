7a6cdcf9c59934570aff8d90c230ae5c20487ae0
ssengup <ssengup@gmail>
2022-03-04 19:02:01 +0530

    view is configured for the register

--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
 # Create your views here.
 
 def home(request):
@@ -19,5 +19,21 @@ def signinpage(request):
 
 
 def register(request):
+    if request.method == 'POST' :
+        username = request.POST['user']
+        password1 = request.POST['pass1']
+        password2 = request.POST['pass2']
+        email = request.POST['email']
 
+
+        if password1 == password2 :
+            if User.objects.filter(username = username):
+                return redirect('register')
+
+            else :
+                user = User.objects.create_user(username = username , password = password1 , email = email)
+                user.save()
+                return redirect('/')
+        else:
+            return redirect('register')
     return render(request , 'register.html')
